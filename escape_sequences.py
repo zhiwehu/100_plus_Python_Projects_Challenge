299db49adb3e8e1650f9ff9462a3dbbfbb7366d1
Vadali Sai Krishna <saikrishnavadali05@gmail.com>
2022-06-29 21:08:59 +0530

    Add files via upload

---
+++ escape_sequences.py
@@ -0,0 +1,12 @@
+# import sys
+
+print(" python \b training : backspace demo ")
+print(" python \n training : newline demo ")
+print(" python \t training : tab demo ")
+print(" python \r training : return demo ")
+print(" python \a training : bell demo ")
+print(" python \\ training : backslash")
+print(" python \" training : double quote")
+print(" python \' training : single quote")
+
+# print("om sri sai \a ram")
